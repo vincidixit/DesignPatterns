f51612086df69d114c54142b7c7aab313e2f1acc
Vinayak Dixit <vincidixit@gmail.com>
2025-03-03 00:45:44 +0530

    Added Singleton Pattern Example

---
+++ CreationalPatterns/Singleton/CacheManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class CacheManager
+    {
+        private readonly MemoryCache cache;
+        private static CacheManager instance;
+
+        private CacheManager()
+        {
+            cache = new MemoryCache("CustomCache");
+        }
+
+        public static CacheManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new CacheManager();
+                }
+                return instance;
+            }
+        }
+
+        public void Add(string key, object value, double expirationInMinutes = 10)
+        {
+            cache.Add(new CacheItem(key, value), new CacheItemPolicy() { AbsoluteExpiration = DateTime.Now.AddMinutes(expirationInMinutes) });
+        }
+
+        public CacheItem Get(string key)
+        {
+            return cache.GetCacheItem(key);
+        }
+
+        public void Remove(string key)
+        {
+            cache.Remove(key);
+        }
+    }
+}
---
+++ CreationalPatterns/Singleton/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Runtime.Caching;
+
+namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CacheManager cacheManager = CacheManager.Instance;
+
+            cacheManager.Add("foo", "bar");
+
+            CacheItem cacheItem = cacheManager.Get("foo");
+
+            if(cacheItem != null)
+            {
+                Console.WriteLine("Cached value: " + cacheItem.Value);
+            }
+        }
+
+    }
+}
